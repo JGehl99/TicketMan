a1944e3d60c68e6aa9aafd8b167d91d798192bde
JGehl99 <jgehl99@gmail.com>
2019-11-05 10:39:03 -0500

    Added example field into !help

--- commands/help.js
+++ commands/help.js
@@ -3,6 +3,7 @@ module.exports = {
     description: "Lists all commands with description and usage",
     usage: "",
     roles: "",
+    example:"!help createroom",
     execute(message, config){
         message.channel.send("Help!");
     }
