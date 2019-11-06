86db93777faeaac6583f077679aa6c4b4cee33f6
JGehl99 <jgehl99@gmail.com>
2019-11-06 17:40:38 -0500

    Command now creates a channel
    Channel will be added under the BOT category
    
    Channel is named from arguments of command

--- commands/createroom.js
+++ commands/createroom.js
@@ -1,13 +1,17 @@
 const Discord = require("discord.js");
 
-
 module.exports = {
     name: "createroom",
-    description: "Creates a temporary role and private text channel and adds message author and user to the role",
+    description: "Creates a temporary role and text channel and adds message author and user to the role",
     usage: "<user>",
-    roles: "Staff",
+    roles: "TECH",
     example: "!createroom Gameproguy",
     execute(client, message, config, command){
+        console.log(message);
         message.channel.send("Create Room!");
+        message.guild.createChannel(message,{type: "text", parent: "641755544549326848"}).then(()=>{
+            console.log;
+        })
+        .catch(console.error);
     }
 }
