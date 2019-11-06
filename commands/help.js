2667928a3d177e614a1d88c81302dd87d8a26c92
Mona Lisa <jgehl99@gmail.com>
2019-11-06 00:11:17 -0500

    Added list all commands Made the formatted message look nicer

--- commands/help.js
+++ commands/help.js
@@ -1,37 +1,52 @@
 const Discord = require("discord.js");
 const config = require("../config.json");
 
+//TODO
+// - Print roles that can use commands when specific command is referenced
+
 module.exports = {
     name: "help",
     description: "Lists all commands with description and usage",
     usage: "",
     roles: "",
-    example:"!help createroom",
+    example: "!help createroom",
     execute(client, message, config, command){
         //console.log(message);
         //console.log(client);
 
+        //console.log(message.content);
+        const args = message.content.slice(config.prefix).split(/ +/);
+        args.shift();
+        console.log(args)
+        console.log(client.commands);
 
-        if (message.content.length > 0){
-
-            //console.log(message.content);
-            const args = message.content.slice(config.prefix).split(/ +/);
-            args.shift();
-            //console.log(args)
-            
-
-            if (!client.commands.has(args[0])){
-                message.reply("Command !" + args[0] + " not found!");
-                return;
-            }
-
-            const cmd = client.commands.get(args[0]);
-
-            
-            console.log(cmd);
-            message.channel.send("Command: " + cmd.name + "\nDescription: " + cmd.description + "\nUsage: " + cmd.usage + "\nExample: " + cmd.example);
-        } else{
-            message.channel.send("eep");
+        //If only !help is called, list all commands available
+        if (args.length == 0){
+            var list = "List of commands: \n";
+            client.commands.forEach(element => {
+                list += "`" + config.prefix + element.name + " " + element.usage + "`\n";
+            });
+            message.channel.send(list);
+            return;
         }
+        
+        //If the inputted command is not found in the list of commands
+        if (!client.commands.has(args[0])){
+            message.reply("Command !" + args[0] + " not found!");
+            return;
+        }
+
+        //gets specific commad based on argument
+        const cmd = client.commands.get(args[0]);
+
+        console.log(cmd);
+
+        //If usage is empty, don't print it
+        if (cmd.usage != 0){
+            message.channel.send("Command: `" + config.prefix + cmd.name + "`\nDescription: `" + cmd.description + "`\nUsage: `" + config.prefix + cmd.name + " " + cmd.usage + "`\nExample: `" + cmd.example + "`");
+        } else {
+            message.channel.send("Command: `" + config.prefix + cmd.name + "`\nDescription: `" + cmd.description  + "`\nExample: `" + cmd.example + "`");
+        }
+
     }
 }
