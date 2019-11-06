390369c2b8b98d7e0b4fdfd9a9cc44b1ecfed47f
JGehl99 <jgehl99@gmail.com>
2019-11-06 18:55:52 -0500

    Added (WIP) ability for certain roles to view chanenl

--- commands/createroom.js
+++ commands/createroom.js
@@ -7,9 +7,41 @@ module.exports = {
     roles: "TECH",
     example: "!createroom Gameproguy",
     execute(client, message, config, command){
+
+        //Command to get user id from mention (to make role for staff and user)
+        //message.mentions.users.first().id
+        //
+        //TODO
+        //
+        // Create a role and add user to it
+        // Change all instances of 'message' using the new id method above
+        //
+        //
+
+
+
         console.log(message);
         message.channel.send("Create Room!");
-        message.guild.createChannel(message,{type: "text", parent: "641755544549326848"}).then(()=>{
+        message.guild.createChannel(message,{
+            type: "text",
+            parent: "641755544549326848",
+            permissionOverwrites: [
+                {
+                    //Users
+                    id: "641330577517314048",
+                    deny: ["VIEW_CHANNEL"]
+                },
+                {
+                    //Staff
+                    id: "641330637000933397",
+                    allow: ["VIEW_CHANNEL"]
+                },
+                {
+                    //Helper
+                    //id: 
+                }
+            ]
+        }).then(()=>{
             console.log;
         })
         .catch(console.error);
