950032ce2495096cc7a08e1f43f510aa8a872978
BuildTools <unconfigured@null.spigotmc.org>
2019-11-12 10:09:29 -0500

    Rewrote my connection methods, and created the ticket creation function. Config.js to be removed with next push

---
+++ sql/config.js
@@ -0,0 +1,9 @@
+let config = {
+  //Should load from a local JSON file, these values are for testing
+    host    : 'localhost',
+    user    : 'NotMyUser',
+    password: 'NotMyPassword',
+    database: 'NotMyDB'
+};
+
+module.exports = config;
--- sql/sql.js
+++ sql/sql.js
@@ -1,22 +1,35 @@
+//Austin Formagin
+//November, 2019
+//TicketMan SQL functions
 const mysql = require('mysql');
+let config = require('./config.js');
 
+let debug = true;
 
-connect();
+if (debug)
+    createTicket("Test Author", "Another testing description!");
 
-/**
- * Establishes connection to the host database
- */
-function connect(){
-    //TODO have username and password load from config file
-    const con = mysql.createConnection({
-        host: "localhost",
-        user: "NotMyUser",
-        password: "NotMyPassword"
+function createTicket(author, desc){
+    //Opens a connection with the Database
+    let connection = mysql.createConnection(config);
+
+    //Get the current time
+    //TODO ^ This <--- Will worry about this later
+    //Set the opening status
+    let o = false;
+
+    let values = [author, desc, o];
+    let ticketInfo = `INSERT INTO tickets (author, description, open) VALUES(?, ?, ?)`;
+    connection.query(ticketInfo, values, (err, results, fields) => {
+        if (err) {
+            return console.error(err.message);
+        }
+        //Prints the ID of the newly created ticket
+        console.log('Created Ticket with ID:' + results.insertId);
     });
 
-    con.connect(function(err) {
-        if (err) throw err;
-        console.log("Connected!");
-    });
+    // console.log("Data added to table");
+    connection.end();
 }
+
 
